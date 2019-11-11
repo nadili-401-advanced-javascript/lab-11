dabd634965c5cf59c9628327da450924410f74b7
Nadya Ilinskaya <nadya.ilinskiy@gmail.com>
2019-11-11 15:14:45 -0800

    starter code

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
 'use strict';
 
 require('dotenv').config();
-
-// Start up DB Server
-const mongoose = require('mongoose');
-const options = {
-  useNewUrlParser:true,
-  useCreateIndex: true,
-};
-mongoose.connect(process.env.MONGODB_URI, options);
-
-// Start the web server
-require('./src/app.js').start(process.env.PORT);
+require('./src/server.js').start(process.env.PORT);
