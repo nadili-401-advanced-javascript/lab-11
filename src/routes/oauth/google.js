6e34d1f103875c4dd871742a4ac7a6f4006b01c2
Nadya Ilinskaya <nadya.ilinskiy@gmail.com>
2019-11-11 16:32:06 -0800

    add comments

--- src/routes/oauth/google.js
+++ src/routes/oauth/google.js
@@ -7,12 +7,11 @@ const router = express.Router();
 
 /**
  * @route GET /google
- * This route authenticates the server to ensure the request is coming from a verified google developer account. We collect the url and options from environment variables, and redirect to our /google-oauth route, which then executes the code from our googleMW module.
+ *  request /google-oauth route 
  * @param {object}   req   The request object
  * @param {object}   res   The response object
- * @param {Function} next  We don't use it in here, but this is our method for going to the next middleware or error middleware in the request-response chain
  * @security OAuth
- * @returns {object} 200 - An authenticated URL to send back to Google's API to retreive user info
+ * @returns {object} 200 -> googleOAuthURL to send back to Google's API to retreive user info
  */
 
 router.get('/google', (req, res, next) => {
@@ -25,32 +24,32 @@ router.get('/google', (req, res, next) => {
     response_type: 'code',
   };
 
-  // TODO: Comment
+  // adding '?' to googleOAuthURL (for adding query later)
   googleOAuthURL += '?';
 
-  // TODO: Comment
+  // combining url query parametrs
   Object.keys(options).forEach((key, indx) => {
     googleOAuthURL += key + '=' + encodeURIComponent(options[key]);
     googleOAuthURL += '&';
   });
 
-  // TODO: Comment
   res.status(200).json({ url: googleOAuthURL });
 });
 
-// TODO: Swagger Comment
+/**
+ * @route GET /google-oauth
+ * @param {object}   req   The request object
+ * @param {object}   res   The response object
+ * @security OAuth
+ * @returns {object} 200 - The name and email address from the selected Google account
+ */
+
 router.get('/google-oauth', async (req, res, next) => {
   let data = await googleMW(req);
 
-  // TODO: Comment
+  // send name and mail as response 
   res.status(200).json({ name: data.name, email: data.email });
 
-  // TODO: README Question:
-  // Now that we have some data about the user, how would we go about
-  // adding this user to our database?
-  // What data should we save?
-  // What data is missing?
-  // What considerations about storing this data do we need to take?
 });
 
 module.exports = router;
